e49dea416210ff8557fe0751f38b465acdb68320
Konstantin Schneider <k.schneider@s-w-e.com>
2023-02-16 15:32:57 +0100

    [update] improvements

--- ctrust-setup.js
+++ ctrust-setup.js
@@ -1,24 +1,24 @@
-import CTrust, { allow, block, options } from './ctrust.min.js'
+import CTrust, { allow, block, options } from './ctrust.min'
 
 const extendOptions = options
 extendOptions.cookies.push(
-	{
-		title: {
-			en: 'Extern Media',
-			de: 'Externe Medien'
-		},
-		description: {
-			en: 'This is my extern media description.',
-			de: 'Dies ist meine Externe Medien Beschreibung.'
-		},
-		checked: true,
-		script: () => {
-			setTimeout(() => {
-				allow()
-			},100)
-		},
-		keys: '_none'
-	}
+  {
+    title: {
+      en: 'Extern Media',
+      de: 'Externe Medien'
+    },
+    description: {
+      en: 'This is my extern media description.',
+      de: 'Dies ist meine Externe Medien Beschreibung.'
+    },
+    checked: true,
+    script: () => {
+      setTimeout(() => {
+        allow()
+      }, 100)
+    },
+    keys: '_none'
+  }
 )
 
 CTrust(extendOptions)
